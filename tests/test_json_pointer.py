e1280c0b9b2c4d349010c0b9c6545afb988de43d
James Prior <jamesgr.prior@gmail.com>
2023-06-13 08:04:01 +0100

    standard uri encoded test cases

--- tests/test_json_pointer.py
+++ tests/test_json_pointer.py
@@ -40,8 +40,29 @@ RFC6901_TEST_CASES = [
     Case(pointer="/m~0n", want=8),
 ]
 
+RFC6901_URI_TEST_CASES = [
+    Case("", want=RFC6901_DOCUMENT),
+    Case("/foo", want=["bar", "baz"]),
+    Case("/foo/0", want="bar"),
+    Case("/", want=0),
+    Case("/a~1b", want=1),
+    Case("/c%25d", want=2),
+    Case("/e%5Ef", want=3),
+    Case("/g%7Ch", want=4),
+    Case("/i%5Cj", want=5),
+    Case("/k%22l", want=6),
+    Case("/%20", want=7),
+    Case("/m~0n", want=8),
+]
+
 
 @pytest.mark.parametrize("case", RFC6901_TEST_CASES)
 def test_rfc6901_examples(case: Case) -> None:
     pointer = JSONPointer(case.pointer)
+    assert pointer.resolve(RFC6901_DOCUMENT) == case.want
+
+
+@pytest.mark.parametrize("case", RFC6901_URI_TEST_CASES)
+def test_rfc6901_uri_examples(case: Case) -> None:
+    pointer = JSONPointer(case.pointer, unicode_escape=False, uri_decode=True)
     assert pointer.resolve(RFC6901_DOCUMENT) == case.want
